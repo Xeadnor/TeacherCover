a6d36dc4da06dbac8ba286fccb9e2c9bac3a4e46
Pablo Sainz <zapencio@gmail.com>
2023-04-22 16:47:25 +0200

    some styles

--- teachercover/src/app/historial-guardias/historial-guardias.component.ts
+++ teachercover/src/app/historial-guardias/historial-guardias.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-historial-guardias',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HistorialGuardiasComponent {
 
+  
+
 }
+
+
