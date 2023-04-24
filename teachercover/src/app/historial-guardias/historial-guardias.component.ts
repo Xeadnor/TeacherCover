be206836ef1dbf4c4f6610bc5567239221cfefc1
Pablo Sainz <zapencio@gmail.com>
2023-04-24 16:18:20 +0200

    filter and sorting

--- teachercover/src/app/historial-guardias/historial-guardias.component.ts
+++ teachercover/src/app/historial-guardias/historial-guardias.component.ts
@@ -1,9 +1,11 @@
 import {Component, ViewChild} from '@angular/core';
 import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
 import {MatPaginatorModule} from '@angular/material/paginator'
-
+import {OnInit,AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { CustomPaginator } from './CustomPaginator';
+import { MatSort } from '@angular/material/sort';
+
 @Component({
 
   selector: 'app-historial-guardias',
@@ -13,25 +15,38 @@ import { CustomPaginator } from './CustomPaginator';
     { provide: MatPaginatorIntl, useValue: CustomPaginator() }
   ]
 })
-export class HistorialGuardiasComponent {
+export class HistorialGuardiasComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
   columnas: string[] = ['codigo', 'descripcion', 'test',"prueba","prueba2","prueba3"];
 
   datos: Articulo[] = [];
   dataSource:any;
 
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
-  
+  @ViewChild(MatSort) sort: MatSort;
   ngOnInit() {
     for (let x = 1; x <= 100; x++)
-      this.datos.push(new Articulo(x, `test ${x}`, "test-test-test-test-test-test","test","test2","test3"));
+    if(x%2 == 0){
+      this.datos.push(new Articulo(x, `test ${x}`, "Martes","test","test2","test3"));
+    }else{
+      this.datos.push(new Articulo(x, `test ${x}`, "Jueves","test","test2","test3"));
+
+    }
     this.dataSource = new MatTableDataSource<Articulo>(this.datos);
     this.dataSource.paginator = this.paginator;
   }
   
+  public doFilter = (event: Event) => {
+    this.dataSource.filter = (event.target as HTMLInputElement).value.trim().toLocaleLowerCase();
+  }
 
 }
 export class Articulo {
   constructor(public codigo: number, public descripcion: string, public test: string, public prueba: string, public prueba2: string, public prueba3: string) {
   }
+
+
 }
 
