27e4ca1c04edd1259dcffe26321bd68f6f6820f2
Pablo Sainz <zapencio@gmail.com>
2023-04-30 18:16:37 +0200

    historialTerminado :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed: :disappointed:

--- teachercover/src/app/historial-guardias/historial-guardias.component.ts
+++ teachercover/src/app/historial-guardias/historial-guardias.component.ts
@@ -11,6 +11,7 @@ import {FormControl,Validators} from '@angular/forms';
 import {from, Observable} from 'rxjs';
 import {distinct, map, mergeMap, startWith, toArray} from 'rxjs/operators';
 import { GuardiaService } from 'app/services/guardia.service';
+import { Guardia } from 'app/models/guardia.model';
 
 @Component({
 
@@ -21,31 +22,39 @@ import { GuardiaService } from 'app/services/guardia.service';
     { provide: MatPaginatorIntl, useValue: CustomPaginator() }
   ]
 })
-export class HistorialGuardiasComponent implements OnInit, AfterViewInit {
-  ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-  }
- guardiaService: GuardiaService
-  columnas: string[] = ['codigo', 'descripcion', 'test',"prueba","prueba2","prueba3"];
+export class HistorialGuardiasComponent implements OnInit {
 
-  datos: Articulo[] = [];
+  columnas: string[] = ['idGuardia', 'profesor', 'curso',"aula","descripcion","diaSemana","estado"];
+  constructor( private guardiaService: GuardiaService) { }
+
+  
+  datos: Guardia[] = [];
   dataSource:any;
+  mostrarTabla: boolean;
   public searchForm: FormGroup;
-  public descripcion = '';
-  public test = '';
-
+  public profesor = '';
+  public curso = '';
+  
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   ngOnInit() {
-    for (let x = 1; x <= 100; x++)
-    if(x%2 == 0){
-      this.datos.push(new Articulo(x, `test ${x}`, "Martes","test","test2","test3"));
-    }else{
-      this.datos.push(new Articulo(x, `test ${x}`, "Jueves","test","test2","test3"));
+    this.mostrarTabla = true;
+    this.guardiaService.getGuardias().subscribe(guardias => {
+      guardias.forEach((guardia) => {
 
-    }
-    
-    this.dataSource = new MatTableDataSource<Articulo>(this.datos);
+        this.datos.push(new Guardia(guardia["diaSemana"], guardia["hora"], guardia["descripcion"], guardia["estado"], guardia["idGuardia"], guardia["aula"], guardia["curso"],guardia["profesor"]));
+      })
+    this.dataSource = new MatTableDataSource<Guardia>(this.datos);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+
+    this.searchFormInit();
+    /* Filter predicate used for filtering table per different columns
+    *  */
+    this.dataSource.filterPredicate = this.getFilterPredicate()
+    });
+
+    this.dataSource = new MatTableDataSource<Guardia>(this.datos);
     this.dataSource.paginator = this.paginator;
 
     this.searchFormInit();
@@ -56,27 +65,27 @@ export class HistorialGuardiasComponent implements OnInit, AfterViewInit {
   }
   searchFormInit() {
     this.searchForm = new FormGroup({
-      descripcion: new FormControl(''),
-      test: new FormControl(''),
+      profesor: new FormControl(''),
+      curso: new FormControl(''),
     });
   }
   getFilterPredicate() {
-    return (row: Articulo, filters: string) => {
+    return (row: Guardia, filters: string) => {
       // split string per '$' to array
       const filterArray = filters.split('$');
-      const descripcion = filterArray[0];
-      const test = filterArray[1];
+      const profesor = filterArray[0];
+      const curso = filterArray[1];
 
       const matchFilter = [];
 
       // Fetch data from row
 
-      const columnDescripcion = row.descripcion;
-      const columnTest = row.test;
+      const columnProfesor = row.profesor;
+      const columnCurso = row.curso;
 
       // verify fetching data by our searching values
-      const customFilterDS = columnDescripcion.toLowerCase().includes(descripcion);
-      const customFilterAS = columnTest.toLowerCase().includes(test);
+      const customFilterDS = columnProfesor.toString().toLowerCase().includes(profesor);
+      const customFilterAS = columnCurso.toLowerCase().includes(curso);
 
       // push boolean values into array
       matchFilter.push(customFilterDS);
@@ -93,22 +102,15 @@ export class HistorialGuardiasComponent implements OnInit, AfterViewInit {
   }
 
   applyFilter() {
-    const as = this.searchForm.get('descripcion')!.value;
-    const ds = this.searchForm.get('test')!.value;
+    const as = this.searchForm.get('profesor')!.value;
+    const ds = this.searchForm.get('curso')!.value;
 
-    this.descripcion = as === null ? '' : as;
-    this.test = ds === null ? '' : ds;
+    this.profesor = as === null ? '' : as;
+    this.curso = ds === null ? '' : ds;
 
     // create string of our searching values and split if by '$'
-    const filterValue = this.descripcion + '$' + this.test;
+    const filterValue = this.profesor + '$' + this.curso;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
-export class Articulo {
-  constructor(public codigo: number, public descripcion: string, public test: string, public prueba: string, public prueba2: string, public prueba3: string) {
-  }
-
-
-}
-
--- teachercover/src/app/services/guardia.service.ts
+++ teachercover/src/app/services/guardia.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
 import { Guardia } from '../models/guardia.model';
 import { Observable } from 'rxjs';
-import { query, where, getDocs } from "firebase/firestore";
+import { query, where, getDocs, getFirestore } from "firebase/firestore";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,10 +11,15 @@ export class GuardiaService {
 
   constructor(private firestore: Firestore) { }
 
+   async getAllGuardias(){
+    const db = getFirestore();
+    const guardiaRef = collection(db,"guardias");
+    const docsSnap = await getDocs(guardiaRef);
+    return docsSnap;
+  }
   getGuardias(): Observable<Guardia[]>{
     const guardiaref = collection(this.firestore,"guardias");
     return collectionData(guardiaref, {idField:"idField"}) as Observable<Guardia[]>;
-    
   }
 
 
